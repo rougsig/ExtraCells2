afc987f56c6638923c55a904887c19b1809f9a71
Andrey Stepankov <rougsig@gmail.com>
2022-02-06 23:28:01 +0200

    Migrate ECGridBlockHardMEDrive from scala to java

---
+++ src/main/scala/extracells/gridblock/ECGridBlockHardMEDrive.java
@@ -0,0 +1,89 @@
+package extracells.gridblock;
+
+import appeng.api.networking.*;
+import appeng.api.util.AEColor;
+import appeng.api.util.DimensionalCoord;
+import extracells.tileentity.TileEntityHardMeDrive;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import java.util.EnumSet;
+
+public class ECGridBlockHardMEDrive implements IGridBlock {
+    private IGrid grid;
+    private int usedChannels;
+    private TileEntityHardMeDrive host;
+
+    public ECGridBlockHardMEDrive(TileEntityHardMeDrive host) {
+        this.host = host;
+    }
+
+    @Override
+    public EnumSet<ForgeDirection> getConnectableSides() {
+        return EnumSet.of(
+                ForgeDirection.DOWN,
+                ForgeDirection.UP,
+                ForgeDirection.NORTH,
+                ForgeDirection.EAST,
+                ForgeDirection.SOUTH,
+                ForgeDirection.WEST
+        );
+    }
+
+    @Override
+    public EnumSet<GridFlags> getFlags() {
+        return EnumSet.of(GridFlags.REQUIRE_CHANNEL, GridFlags.DENSE_CAPACITY);
+    }
+
+    @Override
+    public AEColor getGridColor() {
+        return AEColor.Transparent;
+    }
+
+    @Override
+    public double getIdlePowerUsage() {
+        return this.host.getPowerUsage();
+    }
+
+    @Override
+    public DimensionalCoord getLocation() {
+        return this.host.getLocation();
+    }
+
+    @Override
+    public IGridHost getMachine() {
+        return this.host;
+    }
+
+    @Override
+    public ItemStack getMachineRepresentation() {
+        DimensionalCoord loc = this.getLocation();
+        if (loc == null) return null;
+        return new ItemStack(
+                loc.getWorld().getBlock(loc.x, loc.y, loc.z),
+                1,
+                loc.getWorld().getBlockMetadata(loc.x, loc.y, loc.z)
+        );
+    }
+
+    @Override
+    public void gridChanged() {
+        // no-op
+    }
+
+    @Override
+    public boolean isWorldAccessible() {
+        return true;
+    }
+
+    @Override
+    public void onGridNotification(GridNotification gridNotification) {
+        // no-op
+    }
+
+    @Override
+    public void setNetworkStatus(IGrid iGrid, int i) {
+        this.grid = iGrid;
+        this.usedChannels = i;
+    }
+}
--- src/main/scala/extracells/tileentity/TileEntityHardMeDrive.java
+++ src/main/scala/extracells/tileentity/TileEntityHardMeDrive.java
@@ -10,7 +10,7 @@ import appeng.api.storage.*;
 import appeng.api.util.AECableType;
 import appeng.api.util.DimensionalCoord;
 import extracells.api.IECTileEntity;
-import extracells.gridblock.ECGridBlockHardMEDriveScala;
+import extracells.gridblock.ECGridBlockHardMEDrive;
 import extracells.util.inventory.ECPrivateInventory;
 import extracells.util.inventory.IInventoryUpdateReceiver;
 import net.minecraft.inventory.IInventory;
@@ -30,7 +30,7 @@ public class TileEntityHardMeDrive extends TileBaseScala implements IActionHost,
     byte[] cellStatuses = new byte[3];
     List<IMEInventoryHandler> fluidHandlers = new ArrayList<IMEInventoryHandler>();
     List<IMEInventoryHandler> itemHandlers = new ArrayList<IMEInventoryHandler>();
-    private final ECGridBlockHardMEDriveScala gridBlock = new ECGridBlockHardMEDriveScala(this);
+    private final ECGridBlockHardMEDrive gridBlock = new ECGridBlockHardMEDrive(this);
 
     private ECPrivateInventory inventory = new ECPrivateInventory(
             "extracells.part.drive", 3, 1, this) {
