841e3bf336d68c4df62ea1ef857bd43dc76d9f8d
rougsig <rougsig@gmail.com>
2022-02-07 00:11:27 +0200

    Migrate Nei from scala to java

---
+++ src/main/scala/extracells/integration/nei/Nei.java
@@ -0,0 +1,52 @@
+package extracells.integration.nei;
+
+import codechicken.nei.api.API;
+import extracells.Extracells;
+import extracells.registries.BlockEnum;
+import extracells.registries.ItemEnum;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nei {
+    private Nei() {
+    }
+
+    private static void hideItems() {
+        API.hideItem(new ItemStack(ItemEnum.FLUIDITEM.getItem()));
+        API.hideItem(new ItemStack(ItemEnum.CRAFTINGPATTERN.getItem()));
+        for (ItemEnum item : ItemEnum.values()) {
+            if (item.getMod() != null && (!item.getMod().isEnabled())) {
+                Item i = item.getItem();
+                List<ItemStack> list = new ArrayList<>();
+                i.getSubItems(i, Extracells.ModTab(), list);
+                for (ItemStack itemStack : list) {
+                    API.hideItem(itemStack);
+                }
+            }
+        }
+
+        for (BlockEnum block : BlockEnum.values()) {
+            if (block.getMod() != null && (!block.getMod().isEnabled())) {
+                Block b = block.getBlock();
+                List<ItemStack> list = new ArrayList<>();
+                b.getSubBlocks(Item.getItemFromBlock(b), Extracells.ModTab(), list);
+                for (ItemStack itemStack : list) {
+                    API.hideItem(itemStack);
+                }
+            }
+        }
+    }
+
+    public static void init() {
+        Nei.hideItems();
+        if (Extracells.proxy().isClient()) {
+            UniversalTerminalRecipe handler = new UniversalTerminalRecipe();
+            API.registerUsageHandler(handler);
+            API.registerRecipeHandler(handler);
+        }
+    }
+}
