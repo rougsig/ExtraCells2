c5d2c105a27b7517c14c8425adae4a236e4a6cb9
rougsig <rougsig@gmail.com>
2022-05-02 12:29:34 +0300

    WIP: Add brand new fluid assembler

--- mod/src/main/kotlin/extracells/feature/fluidassembler/FluidAssemblerBlock.kt
+++ mod/src/main/kotlin/extracells/feature/fluidassembler/FluidAssemblerBlock.kt
@@ -1,11 +1,11 @@
-package extracells.feature.fluidcrafter
+package extracells.feature.fluidassembler
 
 import extracells.extension.EMPTY_TILE_ENTITY
 import extracells.feature.ECBaseBlock
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 
-internal class FluidCrafterBlock : ECBaseBlock() {
+internal class FluidAssemblerBlock : ECBaseBlock() {
   override fun createNewTileEntity(world: World, metadata: Int): TileEntity {
     return EMPTY_TILE_ENTITY
   }
