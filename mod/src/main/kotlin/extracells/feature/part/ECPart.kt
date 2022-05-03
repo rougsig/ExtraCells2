f3c0e47515d2d711eec2ef7fe25e0a01fc707aa9
Andrey Stepankov <rougsig@gmail.com>
2022-05-03 19:20:40 +0300

    Add new fluid/export bus (#15)
    * Add brand new fluid export bus
    
    * Add brand new fluid import bus

--- mod/src/main/kotlin/extracells/feature/part/ECPart.kt
+++ mod/src/main/kotlin/extracells/feature/part/ECPart.kt
@@ -1,11 +1,13 @@
 package extracells.feature.part
 
-import extracells.feature.part.fluidexportbus.FluidExportBusPart
+import appeng.parts.AEBasePart
+import extracells.feature.part.fluidbus.FluidExportBusPart
+import extracells.feature.part.fluidbus.FluidImportBusPart
 
 internal enum class ECPart(
   val id: Int,
   val internalName: String,
-  val partClass: Class<out ECPartBase>,
+  val partClass: Class<out AEBasePart>,
 ) {
   FluidExportBus(
     id = 1,
@@ -15,14 +17,14 @@ internal enum class ECPart(
     // generatePair(Upgrades.REDSTONE, 1),
     // generatePair(Upgrades.SPEED, 2)
   ),
-  // FluidImportBus(
-  //   id = 2,
-  //   "fluid.import",
-  //   FluidImportBusPart::class.java,
-  //   // generatePair(Upgrades.CAPACITY, 2),
-  //   // generatePair(Upgrades.REDSTONE, 1),
-  //   // generatePair(Upgrades.SPEED, 2)
-  // ),
+   FluidImportBus(
+     id = 2,
+     "fluid.import",
+     FluidImportBusPart::class.java,
+     // generatePair(Upgrades.CAPACITY, 2),
+     // generatePair(Upgrades.REDSTONE, 1),
+     // generatePair(Upgrades.SPEED, 2)
+   ),
   ;
 
 
