0bf1c9f9b9c5d3d81fa73170d25c73f2f77e1c17
rougsig <rougsig@gmail.com>
2022-05-05 02:06:57 +0300

    Add ae fluid grid core part#2

--- mod/src/main/kotlin/extracells/feature/part/core/ECPartBase.kt
+++ mod/src/main/kotlin/extracells/feature/part/core/ECPartBase.kt
@@ -5,15 +5,41 @@ import appeng.api.implementations.IUpgradeableHost
 import appeng.api.networking.IGridHost
 import appeng.api.networking.security.IActionHost
 import appeng.api.parts.IPart
+import appeng.api.util.DimensionalCoord
+import extracells.helper.EffectiveSide
 import net.minecraft.item.ItemStack
+import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.common.util.ForgeDirection
 
 internal abstract class ECPartBase(
   private val itemStack: ItemStack,
 ) : IPart, IGridHost, IActionHost, IUpgradeableHost, IPowerChannelState {
-  var side: ForgeDirection? = null
+  lateinit var side: ForgeDirection
     private set
 
-  var grid: ECGridBlock? = null
+  lateinit var grid: ECGridBlock
     private set
+
+  private lateinit var tile: TileEntity
+
+  val location: DimensionalCoord
+    get() = DimensionalCoord(this.tile)
+
+  abstract val idlePowerUsage: Double
+
+  init {
+    if (EffectiveSide.isServerSide) {
+      this.grid = ECGridBlock(this)
+    }
+
+    // TODO: continue here
+  }
+
+  override fun getTile(): TileEntity {
+    return this.tile
+  }
+
+  override fun addToWorld() {
+
+  }
 }
