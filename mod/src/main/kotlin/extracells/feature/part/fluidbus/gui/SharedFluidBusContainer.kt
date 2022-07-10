29c91de3bbfaca34b4b14f70742d6d420eaf5aaf
rougsig <rougsig@gmail.com>
2022-07-10 19:16:15 +0300

    Update busiofluid.png

--- mod/src/main/kotlin/extracells/feature/part/fluidbus/gui/SharedFluidBusContainer.kt
+++ mod/src/main/kotlin/extracells/feature/part/fluidbus/gui/SharedFluidBusContainer.kt
@@ -16,21 +16,18 @@ internal class SharedFluidBusContainer(
   }
 
   private fun bindSlots() {
-    this.bindPlayerInventory(player.inventory, 0, 100)
+    this.bindPlayerInventory(player.inventory, 0, 102)
     val config = part.config
 
-    addSlotToContainer(
-      OptionalSlotFluidFakeTypeOnly(
-        config,
-        this,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-      )
-    )
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 0, 80, 40, 0, 0, 0))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 1, 80, 40, -1, 0, 1))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 2, 80, 40, 1, 0, 1))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 3, 80, 40, 0, -1, 1))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 4, 80, 40, 0, 1, 1))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 5, 80, 40, -1, -1, 2))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 6, 80, 40, 1, -1, 2))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 7, 80, 40, -1, 1, 2))
+    addSlotToContainer(OptionalSlotFluidFakeTypeOnly(config, this, 8, 80, 40, 1, 1, 2))
   }
 
   override fun isSlotEnabled(groupNum: Int): Boolean {
